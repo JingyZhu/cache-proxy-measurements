3d1520fde0b77d62b99524a5a5d08481df33d2d4
JingyZhu <jingyz@umich.edu>
2019-01-18 17:32:21 -0500

    get crawl data

---
+++ extract_crawled.py
@@ -0,0 +1,14 @@
+import subprocess
+import os
+import json
+from os.path import join
+import shutil
+
+web_list = os.listdir('0')
+for web in web_list:
+    network_dir = os.listdir(join('0', web))
+    network_file = list(filter(lambda x: 'network_' in x, network_dir))
+    newname = network_file[0][network_file[0].find('network_') + len('network_'):]
+    shutil.copy(join('0', web, network_file[0]), join('uniqprev', 'crawl', newname))
+
+print(network_file)
