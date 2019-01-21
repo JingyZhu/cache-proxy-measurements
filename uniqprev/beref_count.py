269da2ba111f1dec9da6a84f4fb2636facafc13a
JingyZhu <jingyz@umich.edu>
2019-01-21 18:26:23 -0500

    add both query-filtered url and original url

--- uniqprev/beref_count.py
+++ uniqprev/beref_count.py
@@ -2,18 +2,24 @@ import json
 import os
 from os.path import join
 import common
+from urllib.parse import urlparse
 
 web_list = os.listdir('crawl')
 
-url_count = {}
+url_count = {'orig': {}, 'filt': {}}
 
 for web in web_list:
     obj = json.loads(open(join('crawl', web), 'r').read())
     for event in obj:
         if event['method'] == 'Network.requestWillBeSent':
-            url = event['params']['request']["url"]
-            if url not in url_count:
-                url_count[url] = 0
-            url_count[url] += 1
+            orig_url = event['params']['request']["url"]
+            parse_result = urlparse(orig_url)
+            filt_url = '{}://{}'.format(parse_result.scheme, parse_result.netloc + parse_result.path)
+            if orig_url not in url_count['orig']:
+                url_count['orig'][orig_url] = 0
+            url_count['orig'][orig_url] += 1
+            if filt_url not in url_count['filt']:
+                url_count['filt'][filt_url] = 0
+            url_count['filt'][filt_url] += 1
 
-common.gen_cdf([list(url_count.values())], 'url_beref.log')
+common.gen_cdf([list(url_count['orig'].values()), list(url_count['filt'].values())], 'url_beref.log')
